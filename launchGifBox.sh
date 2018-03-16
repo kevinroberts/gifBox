cf18a6c4ec7c1612fa6c1266731355cc84771611
Kevin Roberts <kevin.roberts10@gmail.com>
2018-03-16 19:38:00 +0000

    shell script updates

--- launchGifBox.sh
+++ launchGifBox.sh
@@ -6,6 +6,15 @@
 # Description:
 #	Launches the python scripts and page for
 #	running the Gifbox program.
+
+function finish() {
+	pgrep -f 'python3 websocket-server.py' | xargs kill &
+        pgrep -f 'python3 google-voice-service.py' | xargs kill
+	echo "closing Gifbox. bye."
+}
+
+trap finish SIGINT
+
 python3 websocket-server.py &
 python3 google-voice-service.py &
 /usr/bin/chromium-browser --start-fullscreen websocket-client.html
